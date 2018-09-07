34d70e424313eebe5878bf9352103e3e7d27c859
HeikkiHei <heikkihei.ahonen@gmail.com>
2018-09-07 21:58:05 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,10 +10,24 @@ b1 = Brewery.create name:"Koff", year:1897
 b2 = Brewery.create name:"Malmgard", year:2001
 b3 = Brewery.create name:"Weihenstephaner", year:1040
 
-b1.beers.create name:"Iso 3", style:"Lager"
-b1.beers.create name:"Karhu", style:"Lager"
-b1.beers.create name:"Tuplahumala", style:"Lager"
-b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
-b2.beers.create name:"X Porter", style:"Porter"
-b3.beers.create name:"Hefeweizen", style:"Weizen"
-b3.beers.create name:"Helles", style:"Lager"
+beer1 = b1.beers.create name:"Iso 3", style:"Lager"
+beer2 = b1.beers.create name:"Karhu", style:"Lager"
+beer3 = b1.beers.create name:"Tuplahumala", style:"Lager"
+beer4 = b2.beers.create name:"Huvila Pale Ale", style:"Pale Ale"
+beer5 = b2.beers.create name:"X Porter", style:"Porter"
+beer6 = b3.beers.create name:"Hefeweizen", style:"Weizen"
+beer7 = b3.beers.create name:"Helles", style:"Lager"
+
+beer1.ratings.create score:10
+beer1.ratings.create score:25
+beer1.ratings.create score:45
+beer2.ratings.create score:100
+beer3.ratings.create score:-10
+beer3.ratings.create score:10
+beer4.ratings.create score:50
+beer4.ratings.create score:20
+beer5.ratings.create score:10
+beer6.ratings.create score:20
+beer6.ratings.create score:20
+beer6.ratings.create score:20
+beer6.ratings.create score:20
