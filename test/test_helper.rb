4a07065febfc48bb30e6a29a2b05deaf67d6ed70
HeikkiHei <heikkihei.ahonen@gmail.com>
2018-09-21 14:48:58 +0300

    Fix rubocop

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,8 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-class ActiveSupport
-  class TestCase
+class ActiveSupport::TestCase
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
 
