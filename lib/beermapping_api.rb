43976b6745562ed9d21c45a44e54df88b6ae4bd8
HeikkiHei <heikkihei.ahonen@gmail.com>
2018-10-07 19:55:04 +0300

    Use env variable for API key

--- lib/beermapping_api.rb
+++ lib/beermapping_api.rb
@@ -4,22 +4,22 @@ class BeermappingApi
     Rails.cache.fetch(city, expires_in: 1.week) { get_places_in(city) }
   end
 
-
   def self.get_places_in(city)
     url = "http://beermapping.com/webservice/loccity/#{key}/"
 
     response = HTTParty.get "#{url}#{ERB::Util.url_encode(city)}"
     places = response.parsed_response["bmp_locations"]["location"]
 
-    return [] if places.is_a?(Hash) and places['id'].nil?
+    return [] if places.is_a?(Hash) && places['id'].nil?
 
     places = [places] if places.is_a?(Hash)
-    places.map do | place |
+    places.map do |place|
       Place.new(place)
     end
   end
 
   def self.key
-    "4d44787e6e06e3a8265979d30c5c10ff"
+    raise "BEERMAPPING_APIKEY env variable not defined" if ENV['BEERMAPPING_APIKEY'].nil?
+    ENV['BEERMAPPING_APIKEY']
   end
 end
