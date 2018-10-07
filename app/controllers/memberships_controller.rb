d9a7f137c486831545711c8201985fb5d6b4aaf7
HeikkiHei <heikkihei.ahonen@gmail.com>
2018-10-07 22:54:33 +0300

    I'm done

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -59,7 +59,7 @@ class MembershipsController < ApplicationController
   def destroy
     @membership.destroy
     respond_to do |format|
-      format.html { redirect_to memberships_url, notice: 'Membership was successfully destroyed.' }
+      format.html { redirect_to current_user, notice: "Membership to #{@membership.beer_club.name} was ended." }
       format.json { head :no_content }
     end
   end
